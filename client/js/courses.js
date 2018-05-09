0ece954848f3eaf2cdfb7bed72cb13c6a8ff644b
William <doapps@Williams-Mac-mini.local>
2018-05-09 14:13:55 -0500

    Corregir js

--- client/js/courses.js
+++ client/js/courses.js
@@ -1,40 +1,102 @@
 $(document).on('ready', function(event) {
 
 	function items_toggle() {
-		$('.courses-temary-item-bottom').slideUp();
+		console.log('pantallas menor a 768');			
+		
+		$('.courses-temary-item-bottom').stop(false).slideUp();
+
+		$('.courses-temary-item.first').find('.courses-temary-item-top').addClass('active');
+		$('.courses-temary-item.first').find('.courses-temary-item-bottom').stop(false).slideDown();
 		
 		$(".courses-temary-item-top").on('click', function(event) {
 			$('.courses-temary-item-top').removeClass('active');			
-			$('.courses-temary-item-bottom').slideUp();
-			$(this).addClass('active');
-			$(this).closest('.courses-temary-item').find('.courses-temary-item-bottom').slideDown();
-			/*$this = $(this);
+			$('.courses-temary-item-bottom').stop(false).slideUp();
+			
 	
-			$contentItem = $this.siblings('.courses-temary-item-bottom');
-	
-			if($contentItem.is(":visible")){
-	
-				$this.find("h5 i").removeClass().addClass('icon-chevron-thin-left');
-				$contentItem.slideUp();
+			if( $(".courses-temary-item-top").hasClass('active') ){
+				console.log('este esta abierto');
+				$(this).removeClass('active');
+				$(this).closest('.courses-temary-item').find('.courses-temary-item-bottom').stop(false).slideUp();
+			}else{
+				console.log('este esta cerraod');
+				
+				$(this).addClass('active');
+				$(this).closest('.courses-temary-item').find('.courses-temary-item-bottom').stop(false).slideDown();
 			}
-			else{
-				$this.find("h5 i").removeClass().addClass('icon-angle-double-down');
-				$contentItem.slideDown();
-			}*/
-	
 		});
+
+		
+
+		
+
+			
+		/*$('.courses-temary-item-bottom').stop(false).slideUp();
+		$('.courses-temary-item.first').find('.courses-temary-item-bottom').stop(false).slideDown();
+		$('.courses-temary-item.first').find('.courses-temary-item-top').addClass('active');
+		
+		var toggleTop = $('.courses-temary-item-top'),
+			contador = 0;
+		
+
+		toggleTop.click(function() {
+			console.log('entre');
+			$('.courses-temary-item-top').removeClass('active');
+			$('.courses-temary-item').removeClass('active');
+			$('.courses-temary-item-bottom').stop(false).slideUp();
+			$(this).closest('.courses-temary-item').find('.courses-temary-item-bottom').stop(false).slideDown()
+			$(this).closest('.courses-temary-item').addClass('active')
+			if(contador === 0){
+				$(this).closest('.courses-temary-item').find('.courses-temary-item-top').addClass('active');
+				contador = 1;
+			}else if(contador === 1){
+				$(this).closest('.courses-temary-item').find('.courses-temary-item-top').removeClass('active');
+				contador = 0;
+			}
+		})*/
+	}
+	function items_desactive_toggle(){
+		console.log('pantallas mayor a 768');
+
+		
+		
+		$('.courses-temary-item.first').find('.courses-temary-item-bottom').stop(false).slideDown();
+		$('.courses-temary-item.first').find('.courses-temary-item-top').removeClass('active');
+		
+		$('.courses-temary-item-top').click(function(){
+			$('.courses-temary-item-bottom').stop(false).slideDown();
+			$(this).closest('.courses-temary-item').find('.courses-temary-item-bottom').slideDown();
+
+			$('.courses-temary-item.first').find('.courses-temary-item-bottom').stop(false).slideDown();
+			$('.courses-temary-item.first').find('.courses-temary-item-top').removeClass('active');
+			
+		});
+
+		$('courses-temary-item-bottom').css('display', 'block');
+	} 
+
+
+
+	if (matchMedia) {
+		var mq = window.matchMedia("(max-width: 768px)");
+		mq.addListener(WidthChange);
+		WidthChange(mq);
 	}
 
-	function media_768(){
-		if( window.matchMedia("(max-width: 768px)").matches ){
+	// media query change
+	function WidthChange(mq) {
+
+		if (mq.matches ) {
+			console.log('menor a 768');
 			items_toggle();
-		}
-	}
 
-	window.addEventListener('resize', function(event){
-		media_768();
-	});
-	media_768();
+		}
+		else {
+			console.log('mayor a 768');			
+			items_desactive_toggle();
+			
+		};
+
+	}
 
 });
 
